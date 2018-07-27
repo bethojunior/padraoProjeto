8c967a833df3913ffc9eafa35e3e98df4864d716
betho <hgomescostajr@gmail.com>
2018-07-26 23:02:30 -0300

    baseDao

--- mvc/dao/UserDao.php
+++ mvc/dao/UserDao.php
@@ -1,56 +1,61 @@
 <?php
-    
+class UserDao extends BaseDao{
 
-    class UserDao{
-    
-    
-        public function getUser($email , $pass){
-            try{
-                $conn = Connection::open(Connection::DB_MYSQL);
-                $query = "SELECT * FROM users where email = :email AND pass = :pass";
-                $query = $conn->prepare($query);
-                $query->bindValue(':email' , $email , PDO::PARAM_STR);
-                $query->bindValue(':pass' ,  $pass ,  PDO::PARAM_STR);
-                $query->execute();
-                $all = $query->fetchAll(PDO::FETCH_OBJ);
+    public function getUser($email , $pass){
+        try{
+            $query = "SELECT * FROM users where email = :email AND pass = :pass";
+            $query = $this->ConnectionDb()->prepare($query);
+            $query->bindValue(':email' , $email , PDO::PARAM_STR);
+            $query->bindValue(':pass' ,  $pass ,  PDO::PARAM_STR);
+            $query->execute();
+            $all = $query->fetchAll(PDO::FETCH_OBJ);
 
-                if($query){
-                    echo json_encode($all);
-                }else {
-                    echo json_encode($all);
-                }
-
-            }catch(PDOException $e){
-                echo false;
+            if($query->rowCount() != 0){
+                echo json_encode(array(
+                    "status" => true,
+                    "data" => $all,
+                    "message" => "Cliente encontrado",
+                ));
+                return;
             }
-        }
-
-        public function insertUser($nome , $email , $pass , $typeUser){
+            echo json_encode(array(
+                "status" => false,
+                "data" => $all,
+                "message" => "Dados não conferem",
+            ));
             
-            try{
-                $conn = Connection::open(Connection::DB_MYSQL);
-                $query = ("INSERT INTO users (nome , email , pass , typeUser) VALUES (:nome , :email , :pass , :typeUser)");
-                $query = $conn->prepare($query);
-                $query -> bindValue(':nome' , $nome , PDO::PARAM_STR);
-                $query -> bindValue(':email' , $email , PDO::PARAM_STR);
-                $query -> bindValue(':pass' , $pass , PDO::PARAM_STR);
-                $query -> bindValue(':typeUser' , $typeUser , PDO::PARAM_INT);
-                
-                $query->execute();
+            
+        }catch(PDOException $e){
+            echo false;
+        }
+    }
 
-                if($query){
-                    return json_encode(['result' => true]);
-                }else {
-                    return json_encode(['result' => false]);
-                }
+    public function insertUser($nome , $email , $pass , $typeUser){
+        
+        try{
+            $query = ("INSERT INTO users (name , email , pass , typeUser , tel) VALUES (:name , :email , :pass , :typeUser , :tel)");
+            $query = $this->ConnectionDb()->prepare($query);
+            $query -> bindValue(':name' , $name , PDO::PARAM_STR);
+            $query -> bindValue(':email' , $email , PDO::PARAM_STR);
+            $query -> bindValue(':pass' , $pass , PDO::PARAM_STR);
+            $query -> bindValue(':typeUser' , $typeUser , PDO::PARAM_INT);
+            $query -> bindValue(':tel' , $tel , PDO::PARAM_STR);
+            
+            $query->execute();
 
-            }catch(Exception $e){
+            if($query){
+                return json_encode(['result' => true]);
+            }else {
                 return json_encode(['result' => false]);
             }
 
+        }catch(Exception $e){
+            return json_encode(['result' => false]);
         }
 
-
-        
-    
     }
+
+
+    
+
+}
